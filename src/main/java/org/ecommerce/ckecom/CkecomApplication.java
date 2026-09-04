5c5457aa1c8df0874f34a201ae818a4e391ea7ae
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2026-09-04 11:04:02 +0530

    Initial Commit

---
+++ src/main/java/org/ecommerce/ckecom/CkecomApplication.java
@@ -0,0 +1,13 @@
+package org.ecommerce.ckecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkecomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CkecomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ecommerce/ckecom/CkecomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecommerce.ckecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkecomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
